213a0dad333977846e5cd946a6d9f86599887066
AleksySzlapa <aleksysz069@gmail.com>
2025-05-09 02:36:18 +0200

    Update routes
    made the routes print info that the pages arent done yet

---
+++ front_end_proj/app/routes/logout.tsx
@@ -0,0 +1,11 @@
+import { useRef, useState } from 'react';
+
+export default function Home() {
+  return (
+    <>
+        <div className='bg-black'>
+            This page is not done yet
+        </div>
+    </>
+  );
+}
--- front_end_proj/app/routes/map.tsx
+++ front_end_proj/app/routes/map.tsx
@@ -3,7 +3,9 @@ import { useRef, useState } from 'react';
 export default function Home() {
   return (
     <>
-    
+        <div className='bg-black'>
+            This page is not done yet
+        </div>
     </>
   );
 }
