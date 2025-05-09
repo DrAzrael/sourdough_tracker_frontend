5ff83c79596e256e18e64fb3eb122b334ddd19b8
AleksySzlapa <aleksysz069@gmail.com>
2025-05-09 02:38:03 +0200

    Fix Flash Bang

--- front_end_proj/app/routes/more-info.tsx
+++ front_end_proj/app/routes/more-info.tsx
@@ -3,7 +3,7 @@ import { useRef, useState } from 'react';
 export default function Home() {
   return (
     <>
-        <div className='bg-black'>
+        <div className='bg-black h-full'>
             This page is not done yet
         </div>
     </>
